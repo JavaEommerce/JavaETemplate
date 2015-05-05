b84949bfb71296c0d92340c6076b5a72ff95783c
Nicolas1991 <nicolas19911217@hotmail.com>
2015-05-05 21:52:17 +0100

    problem solved
    异常：java.sql.SQLException: Before start of result set
    
    解决方法：使用rs.getString();前一定要加上rs.next();
    
    原因：ResultSet对象代表SQL语句执行的结果集，维护指向其当前数据行的光标。每调用一次next()方法，光标向下移动一行。最初它位于第一
    行之前，因此第一次调用next()应把光标置于第一行上，使它成为当前行。随着每次调用next()将导致光标向下移动一行。在ResultSe对象及
    其t父辈Statement对象关闭之前，光标一直保持有效。
    
    ps大家一定记得加哦～

--- LoginExample/src/loginSystem/Signup.java
+++ LoginExample/src/loginSystem/Signup.java
@@ -44,9 +44,7 @@ public class Signup extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-//		response.setContentType("text/html");
-//		PrintWriter out = response.getWriter();
-//		out.println("<font size = '6' color = red>"+"Hellor world" +"</font>");
+
 		
 		String userName = request.getParameter("username");
         String password = request.getParameter("password");
@@ -72,100 +70,154 @@ public class Signup extends HttpServlet {
             rd.include(request, response);
         }
         else{
-         
+        	
         	Dbconnection db=null;
-			try {
-				db = new Dbconnection();
-			} catch (ClassNotFoundException e1) {
+    		try {
+    			db = new Dbconnection();
+    		} catch (ClassNotFoundException e1) {
+    			// TODO Auto-generated catch block
+    			e1.printStackTrace();
+    		} catch (SQLException e1) {
+    			// TODO Auto-generated catch block
+    			e1.printStackTrace();
+    		}
+    		Connection con = db.getConnection();
+    		
+    		if (con==null) {
+    			System.out.println("it's closed!");
+    		}
+    		else{
+    			System.out.println("successfullllllll");
+    		}
+        	
+    		try {
+    			
+				insertUser(response, request, userName, password,con);
+				insertAuthor(response, request, userName, password, con);
+				
+			} catch (SQLException e) {
 				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				e.printStackTrace();
 			}
-			Connection con = db.getConnection();
-			
-			if (con==null) {
-				System.out.println("it's closed!");
-			}
-			else{
-				System.out.println("successfullllllll");
-			}
-			
-			PreparedStatement ps = null;
-			PreparedStatement psLookup = null;
-			PreparedStatement psForAuthor = null;
-			PreparedStatement psLookupID = null;
-			ResultSet rs =null;
-			
-	        try {
+    		
+    		System.out.println("register succeed");
+	          //forward to index page
+	            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
+	            PrintWriter out= response.getWriter();
+	            out.println("<font color=green>Registration successful, please login</font>");
+	            out.println("<p>Back Home: <a href=\"http://localhost:8080/JavaEE/index.jsp\">click me</a></p>");
+	            rd.include(request, response);
+				System.out.println("what?");
+        	
+    		
+        	
+	   }
+		
+	}
+	
+	private void insertUser(HttpServletResponse response,HttpServletRequest request,String userName,String password,Connection con) throws ServletException, IOException,SQLException {
+		
+		
+		PreparedStatement ps = null;
+		PreparedStatement psLookup = null;
+		ResultSet rs =null;
+		
+        try {
+            
+        	psLookup = con.prepareStatement("select username from User where username=? limit 1");
+        	psLookup.setString(1, userName);
+        	rs = psLookup.executeQuery();
+        	if (rs!=null&&rs.next()) {
+        		System.out.println(userName);
+        		//forward to login page to login
+	            RequestDispatcher rd = getServletContext().getRequestDispatcher("/signup.jsp");
+	            PrintWriter out= response.getWriter();
+	            out.println("<font color=green>This userName is taken, please change another one.</font>");
+	            rd.include(request, response);
 	            
-	        	psLookup = con.prepareStatement("select username from User where username=? limit 1");
-	        	psLookup.setString(1, userName);
-	        	rs = psLookup.executeQuery();
-	        	if (rs!=null&&rs.next()) {
-	        		System.out.println(userName);
-	        		//forward to login page to login
-		            RequestDispatcher rd = getServletContext().getRequestDispatcher("/signup.jsp");
-		            PrintWriter out= response.getWriter();
-		            out.println("<font color=green>This userName is taken, please change another one.</font>");
-		            rd.include(request, response);
-		            
-				} else {
-					ps = con.prepareStatement("insert into User(ID, username, password,role) values (?,?,?,?)");
-					ps.setString(1, null);
-		            ps.setString(2, userName);
-		            ps.setString(3, password);
-		            ps.setInt(4, 1);
-		            ps.execute();
-		            
-		            
-//		            psLookupID = con.prepareStatement("select ID from User where username=? limit 1");
-//		            psLookupID.setString(1, userName);
-//		            rs =  psLookupID.executeQuery();
-//		            int userID = rs.getInt("ID");
-		            
-		            
-		            
-		            
-		            
-		            psForAuthor = con.prepareStatement("insert into Author(authorname, email, submitstate,ID) values (?,?,?,?)");
-		            psForAuthor.setString(1, userName);
-		            psForAuthor.setString(2,"sample@sheffield.ac.uk");
-		            psForAuthor.setInt(3,1);
-		            psForAuthor.setInt(4,1);
-		            psForAuthor.execute();
-		            
-		            System.out.println("register succeed");
-		          //forward to index page
-		            RequestDispatcher rd = getServletContext().getRequestDispatcher("/signup.jsp");
-		            PrintWriter out= response.getWriter();
-		            out.println("<font color=green>Registration successful</font>");
-		            out.println("<p>Back Home: <a href=\"http://localhost:8080/JavaEE/index.jsp\">click me</a></p>");
-		            rd.include(request, response);
-					System.out.println("what?");
+			} else {
+				ps = con.prepareStatement("insert into User(ID, username, password,role) values (?,?,?,?)");
+				ps.setString(1, null);
+	            ps.setString(2, userName);
+	            ps.setString(3, password);
+	            ps.setInt(4, 1);
+	            ps.execute();
+	           
+			}
+          
+          
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new ServletException("DB Connection problem.");
+        }finally{
+            try {
+               
+            	if (psLookup!=null) {
+					psLookup.close();
 				}
-	          
-	          
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	            throw new ServletException("DB Connection problem.");
-	        }finally{
+            	if (ps!=null) {
+	            	ps.close();
+               }
+               
+            } catch (SQLException e) {
+            	System.out.println("sql exception");
+            }
+        }
+	}
+	
+	private void insertAuthor(HttpServletResponse response,HttpServletRequest request,String userName,String password,Connection con) throws ServletException,IOException {
+		
+		PreparedStatement psForAuthor = null;
+		PreparedStatement psLookupID = null;
+		ResultSet rs =null;
+		
+        try {
+			psLookupID = con.prepareStatement("select ID from User where username=? limit 1");
+			psLookupID.setString(1, userName);
+	        rs =  psLookupID.executeQuery();
+	        if (rs.next()) {
+	        	int userID = rs.getInt("ID");
+	 	        System.out.println(userID);
+			}
+	       
+	        psForAuthor = con.prepareStatement("insert into Author(authorname, email, submitstate,ID) values (?,?,?,?)");
+	        psForAuthor.setString(1, userName);
+	        psForAuthor.setString(2,"sample@sheffield.ac.uk");
+	        psForAuthor.setInt(3,1);
+	        psForAuthor.setInt(4,1);
+	        psForAuthor.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try {
+				if (psLookupID!=null) {
+					psLookupID.close();
+				}
+				if (psForAuthor!=null) {
+					psForAuthor.close();
+				}
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally{
 	            try {
-	               
-	            	if (psLookup!=null) {
-						psLookup.close();
+	                
+	            	if (psLookupID!=null) {
+						psLookupID.close();
 					}
-	            	if (ps!=null) {
-		            	ps.close();
+	            	if (psForAuthor!=null) {
+		            	psForAuthor.close();
 	               }
 	               
 	            } catch (SQLException e) {
 	            	System.out.println("sql exception");
 	            }
 	        }
-	   }
-		
+			
+		}
+        
 	}
 
 }
