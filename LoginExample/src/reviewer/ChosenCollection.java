88c3d0e6b7beb9dd619f035d5ad382c911734e17
Nicolas1991 <nicolas19911217@hotmail.com>
2015-05-05 19:28:43 +0100

    i

--- LoginExample/src/reviewer/ChosenCollection.java
+++ LoginExample/src/reviewer/ChosenCollection.java
@@ -11,4 +11,8 @@ public class ChosenCollection implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private List<ChosenArticle> articleCollection;
 	
+	
+	public ChosenCollection(){
+		
+	}
 }
